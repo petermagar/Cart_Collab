e591a644db2327b21edd49d99764733cedb92a1c
Peter <petermagar4@gmail.com>
2024-03-05 22:54:36 -0500

    Initial Commit

---
+++ CartCollab/CartCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartCollabApp.swift
+//  CartCollab
+//
+//  Created by Peter on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CartCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
